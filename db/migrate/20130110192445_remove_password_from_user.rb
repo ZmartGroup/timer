3c4b2c38eeb1a666a7a33cabd6c5a4f252023a30
Kim Fransman <kim.fransman@gmail.com>
2013-01-10 20:25:40 +0100

    removed old columns from user

---
+++ db/migrate/20130110192445_remove_password_from_user.rb
@@ -0,0 +1,6 @@
+class RemovePasswordFromUser < ActiveRecord::Migration
+  def change
+  	remove_column :users, :crypted_password
+  	remove_column :users, :salt
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130110163530) do
+ActiveRecord::Schema.define(:version => 20130110192445) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -20,12 +20,10 @@ ActiveRecord::Schema.define(:version => 20130110163530) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name",             :null => false
+    t.string   "name",         :null => false
     t.string   "email"
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "github_uid"
     t.string   "github_token"
     t.string   "avatar"
